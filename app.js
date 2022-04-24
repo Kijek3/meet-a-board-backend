9fff50c4986dcaf25ae1cce18be3daf8a1323b91
Filip Szymeczko <szymeczkofil@gmail.com>
2022-04-24 14:52:00 +0200

    feat: cors

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 require('./config/database').connect();
 
 const express = require('express');
+const cors = require('cors');
 const authRouter = require('./routes/authRouter');
 const userRouter = require('./routes/userRouter');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/user', userRouter);
 app.use('/auth', authRouter);
 
