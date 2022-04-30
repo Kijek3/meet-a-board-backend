ab073abbb8c079d843178a08bc9e3cb2a24160fb
Filip Szymeczko <szymeczkofil@gmail.com>
2022-04-30 21:04:07 +0200

    fix: change route type

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const authController = require('../controllers/authController');
 
-router.post('/register', authController.register);
+router.put('/register', authController.register);
 router.post('/login', authController.login);
 router.get('/checkToken', auth, authController.checkToken);
 
