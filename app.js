4df3045c04cf51a03dc7a62e22ccb752df860271
tomek0123456789 <tomasz.jan.miskowicz@gmail.com>
2022-05-28 17:01:10 +0200

    [PHUB-96] feat: meetings (#9)
    * added new endpoints
    * added models
    * fixed typo

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const cors = require('cors');
 const authRouter = require('./routes/authRouter');
 const userRouter = require('./routes/userRouter');
+const meetingRouter = require('./routes/meetingRouter');
 
 const app = express();
 
@@ -12,5 +13,6 @@ app.use(express.json());
 app.use(cors());
 app.use('/user', userRouter);
 app.use('/auth', authRouter);
+app.use('/meeting', meetingRouter);
 
 module.exports = app;
---
+++ models/meeting.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Game = require('./game');
+
+const meetingSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, required: true },
+  guests: { type: [{ userId: mongoose.Schema.Types.ObjectId }] },
+  title: { type: String, required: true },
+  date: { type: String, required: true },
+  startHour: { type: String, required: true },
+  endHour: { type: String, required: true },
+  city: { type: String, required: true },
+  address: { type: String, required: true },
+  isInPublicPlace: { type: Boolean, required: true },
+  game: { type: Game.schema, required: true },
+  description: String,
+  minPlayers: Number,
+  maxPlayers: Number,
+  gameLanguage: String,
+});
+
+module.exports = mongoose.model('meeting', meetingSchema);
