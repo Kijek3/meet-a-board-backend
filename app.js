0d280d876126847a8876621df27386e275730307
Filip Szymeczko <64635454+Kijek3@users.noreply.github.com>
2022-05-29 15:53:13 +0200

    [PHUB-29] feat: game controllers, use bgg API (#11)

--- app.js
+++ app.js
@@ -2,17 +2,21 @@ require('dotenv').config();
 require('./config/database').connect();
 
 const express = require('express');
+const xmlparser = require('express-xml-bodyparser');
 const cors = require('cors');
 const authRouter = require('./routes/authRouter');
 const userRouter = require('./routes/userRouter');
 const meetingRouter = require('./routes/meetingRouter');
+const gameRouter = require('./routes/gameRouter');
 
 const app = express();
 
 app.use(express.json());
+app.use(xmlparser());
 app.use(cors());
 app.use('/user', userRouter);
 app.use('/auth', authRouter);
 app.use('/meeting', meetingRouter);
+app.use('/game', gameRouter);
 
 module.exports = app;
