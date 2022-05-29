31717e62198e4866e22d0cba00603f2f0a93177d
tomek0123456789 <tomasz.jan.miskowicz@gmail.com>
2022-05-29 12:26:53 +0200

    [PHUB-49][PHUB-50] feat: meetings filters & sort (#10)
    * feat: filter init
    
    * feat: meetings filters
    
    Co-authored-by: Filip Szymeczko <szymeczkofil@gmail.com>

--- controllers/meetingController.js
+++ controllers/meetingController.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require('mongoose');
 const Meeting = require('../models/meeting');
 
 exports.addMeeting = async (req, res) => {
@@ -27,24 +28,68 @@ exports.addMeeting = async (req, res) => {
   }
 };
 
-exports.getFutureMeetings = async (req, res) => {
+exports.getFutureMeetings = async (req, res, next) => {
   try {
-    const meetings = await Meeting.find({ date: { $gte: new Date().toISOString() } })
-      .sort({ date: 1 });
+    const meetingsQuery = Meeting.find();
+    meetingsQuery.find({ date: { $gte: new Date().toISOString() } });
+    if (req.body.filter?.search) {
+      meetingsQuery.find({
+        $or: [
+          { title: { $regex: req.body.filter.search, $options: 'i' } },
+          { 'game.title': { $regex: req.body.filter.search, $options: 'i' } },
+        ],
+      });
+    }
+    if (req.body.filter?.minDate) {
+      meetingsQuery.find({
+        date: { $gte: req.body.filter?.minDate },
+      });
+    }
+    if (req.body.filter?.maxDate) {
+      meetingsQuery.find({
+        date: { $lte: req.body.filter?.maxDate },
+      });
+    }
+    if (req.body.filter?.minPlayers) {
+      meetingsQuery.find({
+        minPlayers: { $gte: req.body.filter?.minPlayers },
+      });
+    }
+    if (req.body.filter?.maxPlayers) {
+      meetingsQuery.find({
+        maxPlayers: { $lte: req.body.filter?.maxPlayers },
+      });
+    }
+    if (req.body.filter?.city) {
+      meetingsQuery.find({
+        city: req.body.filter?.city,
+      });
+    }
+
+    const sortBy = { date: 1 };
+    if (req.body.sortBy) {
+      sortBy[req.body.sortBy.field] = req.body.sortBy.asc ? 1 : -1;
+    }
+    meetingsQuery.sort(sortBy);
+
+    const meetings = await meetingsQuery.exec();
     return res.status(200).json(meetings);
   } catch (err) {
-    console.log(err);
-    return res.status(500);
+    return next(err);
   }
 };
 
-exports.getMeeting = (req, res) => {
+exports.getMeeting = async (req, res) => {
   try {
     const id = req.params.meetingId;
-    if (id == null) {
-      return res.status(404).json({ message: 'Invalid meeting ID, meeting not found' });
+    if (!mongoose.isValidObjectId(id)) {
+      return res.status(404).json({ message: 'Meeting ID must be valid' });
     }
-    return res.status(200).json({ message: 'Meeting found!' });
+    const meeting = await Meeting.findById(id);
+    if (meeting === null) {
+      return res.status(404).json({ message: 'Meeting not found' });
+    }
+    return res.status(200).json(meeting);
   } catch (err) {
     console.log(err);
     return res.status(500);
--- routes/meetingRouter.js
+++ routes/meetingRouter.js
@@ -5,7 +5,7 @@ const auth = require('../middleware/auth');
 const meetingController = require('../controllers/meetingController');
 
 router.put('', auth, meetingController.addMeeting);
-router.get('', auth, meetingController.getFutureMeetings);
+router.post('', auth, meetingController.getFutureMeetings);
 router.get('/:meetingId', auth, meetingController.getMeeting);
 
 module.exports = router;
