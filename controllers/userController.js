98432133266f84f82fb9f838c0b47c1a8f9a1601
tomek0123456789 <tomasz.jan.miskowicz@gmail.com>
2022-05-30 17:10:25 +0200

    [PHUB-23][PHUB-79][PHUB-73] feat: edit user info, delete user (#16)
    *added editing user info
    *added deleting user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const { default: mongoose } = require('mongoose');
 
+const bcryptjs = require('bcryptjs');
 const User = require('../models/user');
 
 exports.getUserInfoById = async (req, res) => {
@@ -22,3 +23,65 @@ exports.getUserInfoById = async (req, res) => {
   }
   return res.status(404).send('User doesn\'t exist');
 };
+
+exports.editUserInfo = async (req, res, next) => {
+  Promise.resolve().then(async () => {
+    const id = req.params.userId;
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      city,
+      dob,
+    } = req.body;
+    if (!(email && password && firstName && lastName && city && dob)) {
+      return res.status(400).send('All input is required');
+    }
+    const check = await User.findById(id);
+    const encryptedPassword = await bcryptjs.hash(password, 10);
+    if (req.user.user_id !== check._id.toString()) {
+      return res.status(403).send('Forbidden');
+    }
+    const editUserQuery = await User.findOneAndUpdate({ _id: id }, {
+      firstName,
+      lastName,
+      email: email.toLowerCase(),
+      password: encryptedPassword,
+      city,
+      dob,
+    });
+    return res.status(200).json(editUserQuery);
+  }).catch(next);
+};
+
+exports.deleteUser = async (req, res, next) => {
+  Promise.resolve().then(async () => {
+    const id = req.params.userId;
+    if (!id) {
+      return res.status(404).send('All input is required');
+    }
+    const check = await User.findById(id);
+    if (req.user.user_id !== check._id.toString()) {
+      return res.status(403).send('Forbidden');
+    }
+    const removedUser = await User.findOneAndRemove({ _id: id });
+    console.log(removedUser);
+    return res.status(200).json(removedUser);
+  }).catch(next);
+};
+
+/* test function */
+// exports.getAllUsers = async (req, res, next) => {
+//   Promise.resolve().then(async () => {
+//     User.find({}, (err, users) => {
+//       const userMap = {};
+//
+//       users.forEach((user) => {
+//         userMap[user._id] = user;
+//       });
+//
+//       res.send(userMap);
+//     });
+//   }).catch(next);
+// };
